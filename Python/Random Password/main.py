c332b0951a252443fcb82c326275517602dd0786
UntoldGam <charliebird373@gmail.com>
2022-11-27 22:51:31 +0000

    update R.Password Length Requirement

--- Python/Random Password/main.py
+++ Python/Random Password/main.py
@@ -2,9 +2,9 @@ from random import shuffle
 characterSet="A B C D E F G H I J K L M N O P Q R S T U V W X Y Z a b c d e f g h i j k l m n o p q r s t u v w x y z 0 1 2 3 4 5 6 7 8 9 - ! \" # $ % & \' ( ) * + , . / : ; < = > ? @ [ \ ] ^ _ ` { | } ~ ".split(" ")
 length=0
 while length == 0:
-    legnth_input=int(input(f"Length of your password (minimum 8, no maximum but larger numbers will take longer based on device specs) \nAnswer: "))
-    if legnth_input < 8:
-        print("Length has to be greater than 8")
+    legnth_input=int(input(f"Length of your password (minimum 12, no maximum but larger numbers will take longer based on device specs) \nAnswer: "))
+    if legnth_input < 12:
+        print("Length has to be greater than 12")
     else:
         length = legnth_input
 passwordList=[]
