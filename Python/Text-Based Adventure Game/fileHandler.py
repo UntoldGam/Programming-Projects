17e5627dbbedd705e8466698bd6d2f09e626fc2b
Gam <93488596+UntoldGam@users.noreply.github.com>
2022-12-06 21:08:27 +0000

    updated filehandler

--- Python/Text-Based Adventure Game/fileHandler.py
+++ Python/Text-Based Adventure Game/fileHandler.py
@@ -1,10 +1,28 @@
 from os.path import isfile 
-from json import dump
+from json import dump, loads
 def newUser(data):
     # data = {"user": ..., "health": ...}
-    if isfile(f"./users/{data.userName}"):
-        file = open(f"./users/{data.userName}")
-        file.write(dump(data))
-        file.close()
-    else:
-        file = open(f"")
+		file = open(f"./users/{data.userName}","w")
+		file.write(dump(data))
+		file.close() 
+		return "User Created"
+
+def saveUser(data):
+	#if isfile(f"./users/{data['userName']}"):
+	file = open(f"./users/{data['userName']}.json","r")
+	content = loads(file.read())
+	newData = {}
+	for key in data:
+		print(content, data[key])
+		if data[key] != content[key]:
+			print("different")
+		newData.update({ f"{key}": data[key] })
+		print(newData)
+			# add to newData using the new data
+	file.close()
+	#file = open(f"./users/{data['userName']}.json","w")
+	#file.close()
+	#else:
+	#	newUser(data)
+data = { "userName": "example", "health": 120, "h_multiplier": 0.2 }
+saveUser(data)
