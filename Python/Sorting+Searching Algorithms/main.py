453e8277372370ae76f29d83127ad4518654dd8a
Charlie <charlesobird+github@outlook.com>
2023-03-06 00:18:34 +0000

    add folder for common algorithms

---
+++ Python/Sorting+Searching Algorithms/main.py
@@ -0,0 +1,9 @@
+from algorithms.searching import linear
+from random import randint
+from time import time
+DATA = [randint(0,30),randint(0,30),randint(0,30),randint(0,30),randint(0,30),randint(0,30)]
+
+start_time = time()
+linear_search(dataSet)
+
+endt
