4961ffd5f4ba17458294930accca11907bc459ff
UntoldGam <charlessbird+github@outlook.com>
2023-07-06 13:08:34 +0100

    Delete Python/Common Algorithms directory

--- Python/Common Algorithms/main.py
+++
@@ -1,9 +0,0 @@
-from algorithms.searching import linear
-from random import randint
-from time import time
-DATA = [randint(0,30),randint(0,30),randint(0,30),randint(0,30),randint(0,30),randint(0,30)]
-
-start_time = time()
-linear_search(dataSet)
-
-endt
